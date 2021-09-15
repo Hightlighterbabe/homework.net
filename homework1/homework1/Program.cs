95a26cd9acea184275d64031ddfcd86b941a6963
Hightlighterbabe <victorialatypova22@gmail.com>
2021-09-15 20:03:02 +0300

    commit1

---
+++ homework1/homework1/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace homework1
+{
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            return CalculatorLauncher.Launch(args);
+        }
+    }
+}
