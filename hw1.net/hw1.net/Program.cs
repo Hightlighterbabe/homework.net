1fec921eb9152ea780078ed6312fa347daafc6b8
Hightlighterbabe <victorialatypova22@gmail.com>
2021-09-15 19:13:29 +0300

    commit1

---
+++ hw1.net/hw1.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hw1.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
