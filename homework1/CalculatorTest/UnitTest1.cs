76bf723e2fa9a0a638d2d6117727ac87bbfff93c
Hightlighterbabe <victorialatypova22@gmail.com>
2021-10-14 13:21:34 +0300

    add fsTests

--- homework1/CalculatorTest/UnitTest1.cs
+++ homework1/CalculatorTest/UnitTest1.cs
@@ -1,7 +1,6 @@
-using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using homework1;
-using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using Microsoft.FSharp.Core;
+using FSLibrary;
 
 namespace CalculatorTest
 {
@@ -11,88 +10,97 @@ namespace CalculatorTest
         [TestMethod]
         public void TestSum()
         {
-            AreEqual(2, FSLibrary.Calculator.Calculate(1, 1, FSLibrary.Calculator.Operation.Plus));
+            Assert.AreEqual(FSharpResult<int, string>.NewOk(2), 
+                              Calculator.Calculate
+                              (FSharpResult<int, string>.NewOk(1),
+                               FSharpResult<int, string>.NewOk(1), 
+                                   Calculator.Operation.Plus));
+        }
+        [TestMethod]
+        public void TestSumDouble()
+        {
+            Assert.AreEqual(FSharpResult<double, string>.NewOk(2), 
+                Calculator.Calculate
+                (FSharpResult<double, string>.NewOk(1),
+                    FSharpResult<double, string>.NewOk(1), 
+                    Calculator.Operation.Plus));
+        }
+        [TestMethod]
+        public void TestSumDecimal()
+        {
+            Assert.AreEqual(FSharpResult<decimal, string>.NewOk(2), 
+                Calculator.Calculate
+                (FSharpResult<decimal, string>.NewOk(1),
+                    FSharpResult<decimal, string>.NewOk(1), 
+                    Calculator.Operation.Plus));
+        }
+        [TestMethod]
+        public void TestSumFloat()
+        {
+            Assert.AreEqual(FSharpResult<float, string>.NewOk(2), 
+                Calculator.Calculate
+                (FSharpResult<float, string>.NewOk(1),
+                    FSharpResult<float, string>.NewOk(1), 
+                    Calculator.Operation.Plus));
         }
         
         [TestMethod]
         public void TestMinus()
         {
-            AreEqual(1, FSLibrary.Calculator.Calculate(3, 2, FSLibrary.Calculator.Operation.Minus));
+            Assert.AreEqual(FSharpResult<int, string>.NewOk(1), 
+                              Calculator.Calculate
+                              (FSharpResult<int, string>.NewOk(3),  
+                               FSharpResult<int, string>.NewOk(2), 
+                                   Calculator.Operation.Minus));
         }
-
+        
         [TestMethod]
         public void TestMultiply()
         {
-            AreEqual(5, FSLibrary.Calculator.Calculate(1, 5, FSLibrary.Calculator.Operation.Multiply));
+            Assert.AreEqual(FSharpResult<int, string>.NewOk(5), 
+                              Calculator.Calculate
+                              (FSharpResult<int,string>.NewOk(1), 
+                               FSharpResult<int, string>.NewOk(5), 
+                                   Calculator.Operation.Multiply));
         }
 
         [TestMethod]
         public void TestDivide()
         {
-            AreEqual(5, FSLibrary.Calculator.Calculate(25, 5, FSLibrary.Calculator.Operation.Divide));
-        }
-
-        [TestMethod]
-        public void TestUnknownOperation()
-        {
-            try
-            {
-                FSLibrary.Calculator.Calculate(1, 5, FSLibrary.Calculator.Operation.Unknown);
-            } catch (Exception exception) {
-                AreEqual("Operation is unknown", exception.Message);
-            }
+            Assert.AreEqual(FSharpResult<int, string>.NewOk(5), 
+                              Calculator.Calculate
+                              (FSharpResult<int, string>.NewOk(25), 
+                               FSharpResult<int, string>.NewOk(5),  
+                                   Calculator.Operation.Divide));
         }
 
         [TestMethod]
         public void TestDivideByZero()
         {
-            try
-            {
-                FSLibrary.Calculator.Calculate(1, 0, FSLibrary.Calculator.Operation.Divide);
-            } catch (Exception exception) {
-                AreEqual("Num2 is zero", exception.Message);
-            }
+            Assert.AreEqual(FSharpResult<int, string>.NewError(FSLibrary.Calculator.DivisionByZero), 
+                              Calculator.Calculate
+                              (FSharpResult<int, string>.NewOk(1),
+                               FSharpResult<int, string>.NewOk(0),
+                                   Calculator.Operation.Divide));
         }
 
         [TestMethod]
         public void TestParseNumber()
         {
-            var resultOfParse = Parser.ToParseNumber("1");
-            AreEqual(1, resultOfParse);
-
-            try
-            {
-                var resultOfParse1 = Parser.ToParseNumber("a");
-            }
-            catch (Exception exception)
-            {
-                AreEqual("Value is not integer", exception.Message);
-            }
+            Assert.AreEqual(FSharpResult<int, string>.NewOk(1), Parser.ParseIntNumber("1"));
+            Assert.AreEqual(FSharpResult<int,string>.NewError(FSLibrary.Parser.ParserOperationFail), 
+                              Parser.ParseIntNumber("a"));
         }
 
         [TestMethod]
         public void TestParseOperation()
         {
-            var resultOfParse = Parser.ParseOperation("+");
-            AreEqual(Calculator.Operation.Plus, resultOfParse);
-            
-            var resultOfParse2 = Parser.ParseOperation("-");
-            AreEqual(Calculator.Operation.Minus, resultOfParse2);
-            
-            var resultOfParse3 = Parser.ParseOperation("*");
-            AreEqual(Calculator.Operation.Multiply, resultOfParse3);
-
-            var resultOfParse4 = Parser.ParseOperation("/");
-            AreEqual(Calculator.Operation.Divide, resultOfParse4);
-
-            try
-            {
-                var resultOfParse1 = Parser.ParseOperation("a");
-            }
-            catch (Exception exception)
-            {
-                AreEqual("Operation is not correct", exception.Message);
-            }
+            Assert.AreEqual(FSharpResult<Calculator.Operation, string>.NewOk(Calculator.Operation.Plus), Parser.ParseOperation("+"));
+            Assert.AreEqual(FSharpResult<Calculator.Operation, string>.NewOk(Calculator.Operation.Minus), Parser.ParseOperation("-"));
+            Assert.AreEqual(FSharpResult<Calculator.Operation, string>.NewOk(Calculator.Operation.Multiply), Parser.ParseOperation("*"));
+            Assert.AreEqual(FSharpResult<Calculator.Operation, string>.NewOk(Calculator.Operation.Divide), Parser.ParseOperation("/"));
+            Assert.AreEqual(FSharpResult<Calculator.Operation, string>.NewError(FSLibrary.Parser.ParserOperationFail),
+                              Parser.ParseOperation("a"));
         }
     }
 }
--- homework4f/CalculatorFS.fs
+++ homework4f/CalculatorFS.fs
@@ -2,6 +2,16 @@ namespace FSLibrary
 
 open System
 
+
+type ResultBuilder(errormessage: string) =
+    member b.Zero() = Error errormessage
+    
+    member b.Bind(x, f) =
+        match x with
+        |Ok x -> f x
+        |Error x -> Error x
+    member b.Return(x)= Ok x
+    
 module Calculator=
     
     type Operation=
@@ -9,20 +19,19 @@ module Calculator=
         | Minus
         | Multiply
         | Divide
-        | Unknown
-    let WrongOperation = "Operation is unknown"
     let DivisionByZero = "Num2 is zero"
-    
-    let Calculate (val1: int) (val2: int) operation =
-        match operation with
-        
-        | Plus -> val1 + val2
-        | Minus -> val1 - val2
-        | Multiply -> val1 * val2
-        | Divide ->
-            try
-                val1 / val2
-            with
-            | :? System.DivideByZeroException -> failwith DivisionByZero
-        | Unknown -> failwith WrongOperation
-    
+    let result = ResultBuilder(DivisionByZero)
+    let inline Calculate (val1: Result<'T, string> when 'T:(static member (+): 'T *'T -> 'T))
+                         (val2: Result<'T, string>)
+                         operation =
+        result{
+            let! val1 = val1
+            let! val2 = val2
+            match operation with
+            | Plus -> return val1 + val2
+            | Minus -> return val1 - val2
+            | Multiply -> return val1 * val2
+            | Divide ->
+                if val2 <> new 'T() then
+                    return val1/val2
+        }
