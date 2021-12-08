237e8dddbb24df481eed77d558f0c77639e04e71
Hightlighterbabe <victorialatypova22@gmail.com>
2021-12-08 22:11:22 +0300

    add hw8tests

--- homework8/NewCalculator.cs
+++ homework8/NewCalculator.cs
@@ -9,17 +9,18 @@ namespace homework8
     {
         public decimal Calculate(CalcArguments args)
         {
-            var num1 = decimal.Parse(args.Val1);
-            var num2 = decimal.Parse(args.Val2);
+            var val1 = decimal.Parse(args.Val1);
+            var val2 = decimal.Parse(args.Val2);
 
             return args.Operation switch
             {
-                "+" => num1 + num2,
-                "-" => num1 - num2,
-                "/" => num1 / num2,
-                "*" => num1 * num2,
+                "+" => val1 + val2,
+                "-" => val1 - val2,
+                "/" => val1 / val2,
+                "*" => val1 * val2,
                 _ => throw new Exception("Wrong operation")
             };
         }
+        
     }
 }
